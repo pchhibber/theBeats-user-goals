6fae8c4ffe107511f5e60bd93b6b12dd177199b8
Paritosh Chhibber <chhibberparitosh@gmail.com>
2017-03-16 10:36:33 +0530

    Update d3tree.js

--- d3tree.js
+++ d3tree.js
@@ -414,7 +414,7 @@ treeJSON = d3.json("flare.json", function(error, treeData) {
             .attr('class', 'nodeCircle')
             .attr("r", 0)
             .style("fill", function(d) {
-                return d._children ? "red" : "#fff";
+                return d._children ? "lightsteelblue" : "#fff";
             });
 
         nodeEnter.append("text")
@@ -461,7 +461,7 @@ treeJSON = d3.json("flare.json", function(error, treeData) {
         node.select("circle.nodeCircle")
             .attr("r", 4.5)
             .style("fill", function(d) {
-                return d._children ? "lightsteelblue" : "#fff";
+                return d._children ? "red" : "#fff";
             });
 
         // Transition nodes to their new position.
